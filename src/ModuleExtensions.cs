2b6de92732e7d4a3bb5283e614a6beb16fc6e8c6
jaxelr <jaxelr@gmail.com>
2019-06-21 17:32:58 -0400

    Added sample module extensions.

---
+++ src/ModuleExtensions.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using Carter.Response;
+using Microsoft.AspNetCore.Http;
+
+namespace ActiveDirectory
+{
+    public static class ModuleExtensions
+    {
+        public static Task RunHandler<TOut>(this HttpResponse res, Func<TOut> handler)
+        {
+            try
+            {
+                var response = handler();
+
+                if (response == null)
+                {
+                    res.StatusCode = 204;
+                    return Task.CompletedTask;
+                }
+
+                res.StatusCode = 200;
+                return res.Negotiate(response);
+            }
+            catch (Exception ex)
+            {
+                res.StatusCode = 500;
+                return res.Negotiate(ex.Message);
+            }
+        }
+    }
+}
