1cafe04ca8d3e5f04ad21bc0351711459d18b1dd
jaxelr <jaxelr@gmail.com>
2019-06-25 14:16:47 -0400

    feat: added more extension methods for usage with diff ops

---
+++ src/Extensions/ModuleExtensions.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Threading.Tasks;
+using Carter.ModelBinding;
+using Carter.Response;
+using Microsoft.AspNetCore.Http;
+
+namespace ActiveDirectory
+{
+    public static class ModuleExtensions
+    {
+        public static Task ExecHandler<TOut>(this HttpResponse res, Func<TOut> handler)
+        {
+            try
+            {
+                var response = handler();
+
+                if (response == null)
+                {
+                    res.StatusCode = 204;
+                    return Task.CompletedTask;
+                }
+
+                res.StatusCode = 200;
+                return res.Negotiate(response);
+            }
+            catch (Exception ex)
+            {
+                res.StatusCode = 500;
+                return res.Negotiate(ex.Message);
+            }
+        }
+
+        public static Task ExecHandler<TOut>(this HttpResponse res, string key, Store store, Func<TOut> handler)
+        {
+            try
+            {
+                var response = store.GetOrSetCache(key, () => handler());
+
+                if (response == null)
+                {
+                    res.StatusCode = 204;
+                    return Task.CompletedTask;
+                }
+
+                res.StatusCode = 200;
+                return res.Negotiate(response);
+            }
+            catch (Exception ex)
+            {
+                res.StatusCode = 500;
+                return res.Negotiate(ex.Message);
+            }
+        }
+
+        public static Task ExecHandler<TIn, TOut>(this HttpResponse res, HttpRequest req, Func<TIn, TOut> handler)
+        {
+            try
+            {
+                var (validationResult, data) = req.BindAndValidate<TIn>();
+
+                if (validationResult.IsValid)
+                {
+                    res.StatusCode = 422;
+                    return res.Negotiate(validationResult.GetFormattedErrors());
+                }
+
+                var response = handler(data);
+
+                if (response == null)
+                {
+                    res.StatusCode = 204;
+                    return Task.CompletedTask;
+                }
+
+                res.StatusCode = 200;
+                return res.Negotiate(response);
+            }
+            catch (Exception ex)
+            {
+                res.StatusCode = 500;
+                return res.Negotiate(ex.Message);
+            }
+        }
+    }
+}
