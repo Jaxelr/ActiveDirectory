f2b9a79bbc1556856b6e706c47cd9a7e3e29bd2d
jaxelr <jaxelr@gmail.com>
2019-06-26 11:49:40 -0400

    fix: added missing entities needed for building the src

---
+++ src/Entities/Internal/RouteDefinition.cs
@@ -0,0 +1,8 @@
+﻿namespace ActiveDirectory.Entities
+{
+    public class RouteDefinition
+    {
+        public static string RoutePrefix => "openapi/ui";
+        public static string SwaggerEndpoint => "/openapi";
+    }
+}
---
+++ src/Modules/MainModule.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using ActiveDirectory.Entities;
+using Carter;
+
+namespace ActiveDirectory.Modules
+{
+    public class MainModule : CarterModule
+    {
+        public MainModule()
+        {
+            Get("/", (req, res, routeData) =>
+            {
+                res.Redirect(RouteDefinition.RoutePrefix);
+
+                return Task.CompletedTask;
+            });
+        }
+    }
+}
