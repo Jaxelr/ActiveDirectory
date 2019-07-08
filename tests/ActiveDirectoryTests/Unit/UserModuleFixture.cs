f43eed2df0f31fe90b04146a05368e8f9f4eab1e
jaxelr <jaxelr@gmail.com>
2019-07-08 12:13:16 -0400

    test: added initial test proj

---
+++ tests/ActiveDirectoryTests/Unit/UserModuleFixture.cs
@@ -0,0 +1,27 @@
+﻿using System.Net.Http;
+using ActiveDirectory;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.TestHost;
+
+namespace ActiveDirectoryTests.Unit
+{
+    public class UserModuleFixture
+    {
+        private readonly HttpClient client;
+
+        public UserModuleFixture()
+        {
+            var featureCollection = new FeatureCollection();
+            featureCollection.Set<IServerAddressesFeature>(new ServerAddressesFeature());
+
+            var server = new TestServer(WebHost.CreateDefaultBuilder()
+                    .UseStartup<Startup>(), featureCollection
+            );
+
+            client = server.CreateClient();
+        }
+    }
+}
