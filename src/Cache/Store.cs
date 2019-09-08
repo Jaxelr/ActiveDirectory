4f252da4db817a4729cd01f5edefe2880eba72d4
Jaxel Rojas <jaxelr@gmail.com>
2019-09-08 08:57:10 -0400

    fix: change sligin expiration for absolute expiration

--- src/Cache/Store.cs
+++ src/Cache/Store.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ActiveDirectory.Entities;
 using Microsoft.Extensions.Caching.Memory;
 
 namespace ActiveDirectory
@@ -8,12 +9,19 @@ namespace ActiveDirectory
         private readonly IMemoryCache cache;
         private readonly CacheConfig props;
 
-        public Store(IMemoryCache cache, CacheConfig props)
+        public Store(IMemoryCache cache, AppSettings appSettings)
         {
             this.cache = cache;
-            this.props = props;
+            props = appSettings.Cache;
         }
 
+        /// <summary>
+        /// Gets the cache value from store by key if it misses, it sets the result as cached by the key provided
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key">The cache key to search on the store</param>
+        /// <param name="fn"></param>
+        /// <returns></returns>
         public T GetOrSetCache<T>(string key, Func<T> fn)
         {
             var (res, cacheHit) = GetCache(key, fn);
@@ -24,6 +32,12 @@ namespace ActiveDirectory
             return res;
         }
 
+        /// <summary>
+        /// Set the store using the key provided
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key">The cache key to set the store value</param>
+        /// <param name="res">The T value to store</param>
         public void SetCache<T>(string key, T res)
         {
             if (props.CacheEnabled)
@@ -31,13 +45,20 @@ namespace ActiveDirectory
                 string realKey = Key.Create<T>(key);
 
                 var options = new MemoryCacheEntryOptions()
-                    .SetSlidingExpiration(TimeSpan.FromSeconds(props.CacheTimespan))
+                    .SetAbsoluteExpiration(TimeSpan.FromSeconds(props.CacheTimespan))
                     .SetSize(props.CacheMaxSize);
 
                 cache.Set(realKey, res, options);
             }
         }
 
+        /// <summary>
+        /// Get the value on the store as based on the key, if missed return the result of invoking the Func<t>
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="key"></param>
+        /// <param name="fn"></param>
+        /// <returns></returns>
         public (T, bool) GetCache<T>(string key, Func<T> fn)
         {
             string realKey = Key.Create<T>(key);
