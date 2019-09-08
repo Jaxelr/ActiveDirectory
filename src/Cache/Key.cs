bc4badfb0630d1a40600054d8adb476e041dfd08
Jaxel Rojas <jaxelr@gmail.com>
2019-09-08 09:00:35 -0400

    docs: documented Key class for usage with cache

--- src/Cache/Key.cs
+++ src/Cache/Key.cs
@@ -4,12 +4,30 @@ namespace ActiveDirectory
 {
     public class Key
     {
-        public static string FieldSeparator = ":";
+        public static string FieldSeparator = "::";
 
+        /// <summary>
+        /// Create a string key based on the typeof<T> and the field sent
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="field"></param>
+        /// <returns></returns>
         public static string Create<T>(string field) => Create(typeof(T), field);
 
+        /// <summary>
+        /// Create a string key based on the typeof<T> and the fields sent
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fields"></param>
+        /// <returns></returns>
         public static string Create<T>(params string[] fields) => Create(typeof(T), fields);
 
+        /// <summary>
+        /// Create a string key based on the type and the fields sent
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="fields"></param>
+        /// <returns></returns>
         public static string Create(Type type, params string[] fields)
         {
             if (type == null)
@@ -33,6 +51,12 @@ namespace ActiveDirectory
             return urn;
         }
 
+        /// <summary>
+        /// Create a string key based on the type and the field sent
+        /// </summary>
+        /// <param name="type"></param>
+        /// <param name="field"></param>
+        /// <returns></returns>
         public static string Create(Type type, string field)
         {
             if (type == null)
