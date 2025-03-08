15887d348eab440057c25172ccc471761a797cd6
Jaxel Rojas <jaxelr@gmail.com>
2025-03-08 10:16:13 -0400

    style: update code comments

--- src/Extensions/CollectionExtensions.cs
+++ src/Extensions/CollectionExtensions.cs
@@ -9,7 +9,7 @@ public static class CollectionExtensions
     /// Since the mapping from the DI returns an instantiated class as based on the appsettings.json
     /// config file, we must check if the only element included is not empty, then we can define it as empty.
     /// </summary>
-    /// <param name="collection"></param>
+    /// <param name="collection">Determine if the collecton is empty.</param>
     internal static bool Empty(this IEnumerable<string> collection) =>
         collection.Count() <= 1 && string.IsNullOrEmpty(collection.FirstOrDefault());
 }
--- src/Extensions/HttpContextExtensions.cs
+++ src/Extensions/HttpContextExtensions.cs
@@ -13,7 +13,7 @@ public static class HttpContextExtensions
     /// <summary>
     /// Encapsulate execution of handler with the corresponding validation logic
     /// </summary>
-    /// <typeparam name="TOut"></typeparam>
+    /// <typeparam name="TOut">Type of the output of the handler</typeparam>
     /// <param name="ctx">The http context to process</param>
     /// <param name="handler">A func handler that will be validated and executed</param>
     /// <returns name="Task">A Task object with the results</returns>
@@ -40,7 +40,7 @@ public static class HttpContextExtensions
     /// <summary>
     /// Encapsulate execution of handler with the validation logic and storage on cache using the key provided
     /// </summary>
-    /// <typeparam name="TOut"></typeparam>
+    /// <typeparam name="TOut">Type of the output of the handler</typeparam>
     /// <param name="ctx">The http context to process</param>
     /// <param name="cacheTimespan">Time alive for the store to keep</param>
     /// <param name="handler">A func handler that will be executed</param>
@@ -70,8 +70,8 @@ public static class HttpContextExtensions
     /// <summary>
     /// Encapsulate execution of handler with the validation logic while binding and validating the http request
     /// </summary>
-    /// <typeparam name="TIn"></typeparam>
-    /// <typeparam name="TOut"></typeparam>
+    /// <typeparam name="TIn">Type of the input of the handler</typeparam>
+    /// <typeparam name="TOut">Type of the output of the handler</typeparam>
     /// <param name="ctx">The http context to process</param>
     /// <param name="handler">A func handler that will be validated and executed</param>
     public static async Task ExecHandler<TIn, TOut>(this HttpContext ctx, TIn @in, Func<TIn, TOut> handler)
@@ -110,8 +110,8 @@ public static class HttpContextExtensions
     /// Encapsulate execution of handler with the validation logic while binding,
     /// validating the http request and storing on cache using the key provided
     /// </summary>
-    /// <typeparam name="TIn"></typeparam>
-    /// <typeparam name="TOut"></typeparam>
+    /// <typeparam name="TIn">Type of the input of the handler</typeparam>
+    /// <typeparam name="TOut">Type of the output of the handler</typeparam>
     /// <param name="ctx">The http context to process</param>
     /// <param name="cacheTimespan">Time alive for the store to keep</param>
     /// <param name="handler">A func handler that will be validated and executed</param>
